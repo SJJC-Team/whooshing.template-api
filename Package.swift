6c0bc472def0a110789c8341e4b1540a7ad0fc50
ChenLin Wang <wangchenlin2001@icloud.com>
2025-05-04 13:50:02 +0800

    V0.3.14.13 更新依赖

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // 💧 Vapor -- Swift 服务器端第三方框架
         .package(url: "https://github.com/SJJC-Team/whooshing-vapor.git", from: "1.0.0"),
         // ⭐️ Whooshing 系统基本框架
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", from: "1.2.0"),
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", from: "1.2.1"),
         // 🔵 Swift 高性能网络通讯模块
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         // 🗄 关系型和非关系型数据库的 ORM(对象关系映射)
